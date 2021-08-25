d6a81d9f140db2d347ab9c83c926fb854645f8f7
mixaefes <mixaefes@gmail.com>
2021-08-25 14:55:29 +0300

    add:recycler view adapter and view holder

---
+++ app/src/main/java/com/example/petsapproom/adapter/PetAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.petsapproom.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.example.petsapproom.data.EntityPet
+import com.example.petsapproom.databinding.PetItemViewBinding
+
+class PetAdapter : ListAdapter<EntityPet,PetViewHolder>(itemComparator) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PetViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = PetItemViewBinding.inflate(inflater,parent,false)
+        return PetViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: PetViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+  private  companion object {
+        val itemComparator = object :DiffUtil.ItemCallback<EntityPet>(){
+            override fun areItemsTheSame(oldItem: EntityPet, newItem: EntityPet): Boolean  = oldItem==newItem
+
+            override fun areContentsTheSame(oldItem: EntityPet, newItem: EntityPet): Boolean = oldItem.id==newItem.id
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/petsapproom/adapter/PetViewHolder.kt
@@ -0,0 +1,13 @@
+package com.example.petsapproom.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.petsapproom.data.EntityPet
+import com.example.petsapproom.databinding.PetItemViewBinding
+
+class PetViewHolder(binding:PetItemViewBinding) : RecyclerView.ViewHolder(binding.root) {
+
+fun bind(item:EntityPet){
+    //todo make binding data
+}
+}
